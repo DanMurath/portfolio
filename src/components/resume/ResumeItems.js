2de7a75a7426877e67b0269318fa19dd69d6fbf8
DanMurath <danielmurathodzic@hotmail.com>
2021-03-05 12:50:07 +0000

    bug fixes

---
+++ src/components/resume/ResumeItems.js
@@ -0,0 +1,14 @@
+export const ResumeItems = [
+  {
+    title: "Education",
+    description: `St. David's Sixth Form, BBB [2018-2020], St. Ilttyd's High School, AAAABBB [2013-2018]`,
+  },
+  {
+    title: "Technologies",
+    description: "JavaScript, React, HTML5, CSS3, SCSS(Sass), Materialize, Git",
+  },
+  {
+    title: "Language",
+    description: "English, Italian, Bosnian",
+  },
+];
